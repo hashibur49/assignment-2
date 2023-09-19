02806acba60bbeddaa9670546e4ae99c9322cd30
hashibur49 <hashibur24@gmail.com>
2023-09-19 23:46:19 +0600

    task one

--- taskone.php
+++ taskone.php
@@ -1,2 +1,46 @@
 <?php
-echo "Hello";
+// // function printEvenNumbersUsingFor($start, $end, $step) {
+// //     for ($i = $start; $i <= $end; $i += $step) {
+// //         echo $i;
+// //         if ($i + $step <= $end) {
+// //             echo ', ';
+// //         }
+// //     }
+// // }
+
+// // Call the function to print even numbers from 1 to 20 with a step of 2
+// printEvenNumbersUsingFor(2, 20, 2);
+
+// //while loop
+// function printEvenNumbersUsingWhile($start, $end, $step) {
+//     $i = $start;
+//     while ($i <= $end) {
+//         echo $i;
+//         if ($i + $step <= $end) {
+//             echo ', ';
+//         }
+//         $i += $step;
+//     }
+// }
+
+// // Call the function to print even numbers from 1 to 20 with a step of 2
+// printEvenNumbersUsingWhile(2, 20, 2);
+
+
+//do-while loop
+
+function printEvenNumbersUsingDoWhile($start, $end, $step) {
+    $i = $start;
+    do {
+        echo $i;
+        if ($i + $step <= $end) {
+            echo ', ';
+        }
+        $i += $step;
+    } while ($i <= $end);
+}
+
+// Call the function to print even numbers from 1 to 20 with a step of 2
+printEvenNumbersUsingDoWhile(2, 20, 2);
+
+
